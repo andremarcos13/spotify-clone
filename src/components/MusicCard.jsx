4ca53a738e3e144d5f9414bf957b7e0e9aacb192
Andre Cesar <andrmgc@gmail.com>
2022-04-25 13:42:18 -0300

    adicionado input para favoritar as musicas, utilizando a funcao addSong, esta passando nos testes

--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -1,21 +1,55 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { addSong } from '../services/favoriteSongsAPI';
+import Loading from '../pages/Loading';
 
 class MusicCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+      clickFav: false,
+    };
+    this.handleFavorite = this.handleFavorite.bind(this);
+  }
+
+  async handleFavorite({ target }) {
+    const { data } = this.props;
+    this.setState({ loading: true });
+    if (target.checked) await addSong(data);
+    this.setState({ loading: false, clickFav: target.checked });
+  }
+
   render() {
-    const { trackName, previewUrl } = this.props;
+    const { trackName, previewUrl, trackId } = this.props;
+    const { loading, clickFav } = this.state;
     return (
       <div className="componente-musiccard">
-        Componente Music Card
-        <div>
-          <p>{trackName}</p>
-          <audio data-testid="audio-component" src={ previewUrl } controls>
-            <track kind="captions" />
-            O seu navegador não suporta o elemento
-            <code>audio</code>
-            .
-          </audio>
-        </div>
+        { loading ? <Loading /> : (
+          <>
+            Componente Music Card
+            <div>
+              <h3>{trackName}</h3>
+              <audio data-testid="audio-component" src={ previewUrl } controls>
+                <track kind="captions" />
+                O seu navegador não suporta o elemento
+                <code>audio</code>
+                .
+              </audio>
+              <label htmlFor={ trackId }>
+                Favorita
+                <input
+                  data-testid={ `checkbox-music-${trackId}` }
+                  type="checkbox"
+                  name="fav-song"
+                  id={ trackId }
+                  defaultChecked={ clickFav }
+                  onChange={ this.handleFavorite }
+                />
+              </label>
+            </div>
+          </>
+        )}
       </div>
     );
   }
@@ -24,6 +58,8 @@ class MusicCard extends React.Component {
 MusicCard.propTypes = {
   trackName: PropTypes.string.isRequired,
   previewUrl: PropTypes.string.isRequired,
+  trackId: PropTypes.number.isRequired,
+  data: PropTypes.arrayOf.isRequired,
 };
 
 export default MusicCard;
